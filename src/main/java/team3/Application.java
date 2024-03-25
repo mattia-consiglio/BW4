e47faf2e02d10c4d467191074f4eea43b6067c3d
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-25 16:38:39 +0100

    Started supplier

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -3,12 +3,21 @@ package team3;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import team3.entities.Emettitore;
+import team3.entities.EmettitoreEnum;
+
+import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4t3");
     private static final EntityManager em = emf.createEntityManager();
 
     public static void main(String[] args) {
+        Supplier<Emettitore> emettitoreSupplier = () -> {
+            EmettitoreEnum tipologia = EmettitoreEnum.values().
+            return new Emettitore();
+        };
+
         System.out.println("Hello World!");
         em.close();
     }
