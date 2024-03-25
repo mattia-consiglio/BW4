577076358f66c448a314315fddddfb9beef38ffa
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-25 13:10:10 +0100

    Add db connection + updated persistence.xml

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -1,8 +1,15 @@
 package team3;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4t3");
+    private static final EntityManager em = emf.createEntityManager();
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        em.close();
     }
 }
