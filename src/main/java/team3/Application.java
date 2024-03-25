1332d0ab7877cdf0dba70a0f952b1e9b75b2ccee
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-25 18:43:57 +0100

    Dome emettitoreSupplier

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -1,21 +1,32 @@
 package team3;
 
+import com.github.javafaker.Faker;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import team3.entities.Emettitore;
 import team3.entities.EmettitoreEnum;
+import team3.entities.EmettitoreStato;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4t3");
     private static final EntityManager em = emf.createEntityManager();
 
+    private static final Faker faker = new Faker();
+
     public static void main(String[] args) {
         Supplier<Emettitore> emettitoreSupplier = () -> {
-            EmettitoreEnum tipologia = EmettitoreEnum.values().
-            return new Emettitore();
+            EmettitoreEnum tipologia = EmettitoreEnum.values()[new Random().nextInt(EmettitoreEnum.values().length)];
+            EmettitoreStato stato = null;
+            if (tipologia == EmettitoreEnum.DISTRIBUTORE) {
+                stato = EmettitoreStato.values()[new Random().nextInt(EmettitoreStato.values().length)];
+            }
+            String city = faker.address().cityName();
+
+            return new Emettitore(faker.company().name(), faker.address().streetAddress(), faker.number().digits(3), city, city, faker.number().digits(5), faker.address().country(), tipologia, stato);
         };
 
         System.out.println("Hello World!");
