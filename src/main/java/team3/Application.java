a31041a96390aafadbfdf05faf8895274adc8b76
Fejz7 <147973017+Fejz7@users.noreply.github.com>
2024-03-26 16:16:12 +0100

    Ardit emettitori (#12)
    * changes misc.xml
    
    * added partial mainmenu
    
    * added trim

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -73,7 +73,7 @@ public class Application {
 
 
             String option = scanner.nextLine();
-
+            option =option.trim();
             switch (option) {
                 case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13": {
                     return Integer.parseInt(option);
