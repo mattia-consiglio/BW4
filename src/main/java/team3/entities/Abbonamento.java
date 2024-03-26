bda04db4fdc9187fc1c07f453e871855efb7cca3
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-26 17:05:35 +0100

    Fix relations from previous Merge
    Co-Authored-By: Dalila Giurgola <148708941+dalilagiu98@users.noreply.github.com>

--- src/main/java/team3/entities/Abbonamento.java
+++ src/main/java/team3/entities/Abbonamento.java
@@ -1,8 +1,6 @@
 package team3.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
@@ -12,5 +10,19 @@ public class Abbonamento extends TitoloViaggio {
     private LocalDate dataFine;
     @Enumerated(value = EnumType.STRING)
     private TipoAbbonamento tipoAbbonamento;
+    @ManyToOne
+    @JoinColumn(name = "id_tessera")
+    private Tessera tessera;
 
+    public Abbonamento(LocalDate dataEmissione, Emettitore emettitore, LocalDate dataInizio, LocalDate dataFine, TipoAbbonamento tipoAbbonamento, Tessera tessera) {
+        super(dataEmissione, emettitore);
+        this.dataInizio = dataInizio;
+        this.tipoAbbonamento = tipoAbbonamento;
+        if (this.tipoAbbonamento.equals(TipoAbbonamento.SETTIMANALE)) {
+            this.dataFine = this.dataInizio.plusDays(7);
+        } else {
+            this.dataFine = this.dataInizio.plusDays(30);
+        }
+        this.tessera = tessera;
+    }
 }
